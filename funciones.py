b857ef3a06af3eb2395ac3c9d4c7d21bd68ef548
unknown <Leroy@leroygordo.(none)>
2011-10-20 01:19:19 -0430

    mas de lo que habia jeje

--- funciones.py
+++ funciones.py
@@ -4,7 +4,29 @@ def extraerArgumentos(argumentos):
 	i = 1
 	for a in argumentos[1:]:
 		if(a[0] == '+' or a[0] == '-'):
-			palabras.append(a[1:])
+			palabras.append(a)
 			i = i +1
 	archivos = argumentos[i:]
 	return (palabras,archivos)
+	
+def procesarPalabras(palabras):
+	permitidas = {}
+	prohibidas = []
+	for p in palabras:
+		if(p[0] == '+'):
+			if(p[1:] in permitidas):
+				permitidas[p[1:]] = permitidas[p[1:]] + 1
+			else:
+				permitidas[p[1:]] = 1
+		elif(p[0] == '-' and not(p[1:] in prohibidas)):
+			prohibidas.append(p[1:])
+	return (permitidas,prohibidas)
+	
+def procesarArchivos(archivos):
+	pointer_files = []
+	for f in archivos:
+		try:
+			pointer_files.add(open(f, "r"))		
+		except:
+			print "Error al abrir el archivo '" + f + "'"
+	return pointer_files
--- instancias.py
+++ instancias.py
@@ -1,6 +1,9 @@
 import sys
 import funciones
 
+if('-' in argv)
+	argv.remove('-')
+
 if(len(sys.argv) < 3):
 	print "Faltan parametros"
 	exit
@@ -14,3 +17,7 @@ elif((len(sys.argv) == 3) and (argv[1] != '+' or argv[1] != '-') and
 if(len(palabras) == 0 or len(archivos) == 0)
 	print ("Faltan parametros: debe haber por lo menos una palabra y"
 	       "un archivo")
+
+(dic_permitidas,list_prohibidas) = procesarPalabras(palabras)
+
+files = procesarArchivos(archivos)
