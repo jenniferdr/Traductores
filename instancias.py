c46b24bc8f99e4cd52ac182e9cd26a9dea38315a
Hancel Gonzalez <lyrae@leroygordo.(none)>
2011-10-22 18:09:43 -0430

    exit -> sys.exit(0)

--- instancias.py
+++ instancias.py
@@ -6,60 +6,50 @@ if('-' in sys.argv):
 
 if(len(sys.argv) < 3):
 	print "Faltan parametros"
-	exit
+	sys.exit(0)
 elif((len(sys.argv) == 3) and ((sys.argv[1][0] != '+' and sys.argv[1][0] != '-')
 			       or sys.argv[2][0] == '+' or sys.argv[2][0] == '-')):
 	print "Parametros invalidos"
-	exit
+	sys.exit(0) 
     
 (palabras,archivos) = funciones.extraerArgumentos(sys.argv)
 
 if((len(palabras) == 0) or (len(archivos) == 0)):
 	print ("Faltan parametros: debe haber por lo menos una palabra y"
-	       "un archivo")
+	       " un archivo")
+	sys.exit(0)
     
 files = funciones.procesarArchivos(archivos)    
 
-# Palabras permitidas con el numero de veces que deben aparecer
-global dic_permitidas
-
-# Lista de palabras que no deben aparecer
-# o aparecen un numero exacto de veces 
-global list_prohibidas
+global dic_permitidas # Palabras permitidas con el numero de veces que deben aparecer
+global list_prohibidas # Lista de palabras que no deben aparecer o aparecen un numero exacto de veces 
 
 (dic_permitidas,list_prohibidas) =funciones.procesarPalabras(palabras)
 
-# Lista de letras pertenecientes a las palabras prohibidas
-listaLP= funciones.listaLetras(list_prohibidas)
+listaLP = funciones.listaLetras(list_prohibidas) # Lista de letras pertenecientes a las palabras prohibidas
 
-## dfa= new Graph()
-# DFA formado con las letras de las palabras prohibidas
-dfa= funciones.dfaPalabras(listaLP)
+# dfa = new Graph()
+# dfa = funciones.dfaPalabras(listaLP) # DFA formado con las letras de las palabras prohibidas
 
-# Expresion regular de palabras formadas con las letras
-# de palabras prohibidas. 
-palabras_LP=funciones.convertirA_ER(dfa)
+# palabras_LP=funciones.convertirA_ER(dfa) # Expresion regular de palabras formadas con las letras de palabras prohibidas.
 
 # ER para representar cualquier caracter menos las que aparecen
 # en las palabras prohibidas
-strLNP="[^"
-for l in listaLP:
-    strLP += l
-str += "]"
+#strLNP="[^"
+#for l in listaLP:
+#    strLP += l
+#str += "]"
 
 # ER para representar cualquier palabra menos las prohibidas     
-NP= "("+strLNP+"*("+palabras_LP+strLNP+"+|"+strLNP+")*"+palabras_LP+"?)" 
+#NP= "("+strLNP+"*("+palabras_LP+strLNP+"+|"+strLNP+")*"+palabras_LP+"?)" 
 
 # Con todas sus permutaciones de palabras en diccionario 
-ER=..NP palabra NP palabra.. 
+#ER=..NP palabra NP palabra.. 
 ## compilarla
 
-for file in files:
-    for line in file:
-        if(re.match("^"+ER+"$",line):
-           print line
+#for file in files:
+#    for line in file:
+#        if(re.match("^"+ER+"$",line):
+#           print line
 
-# Y finnn :) 
-
-           
-
+# Y finnn :)
