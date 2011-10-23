0e08017517995ad92ad9bbeab12735dace342648
Jennifer <jenniferdosreis@hotmail.com>
2011-10-22 20:14:28 -0430

    Borron y cuenta nueva

--- instancias.py
+++ instancias.py
@@ -21,27 +21,7 @@ if((len(palabras) == 0) or (len(archivos) == 0)):
     
 files = funciones.procesarArchivos(archivos)    
 
-global dic_permitidas # Palabras permitidas con el numero de veces que deben aparecer
-global list_prohibidas # Lista de palabras que no deben aparecer o aparecen un numero exacto de veces 
-
 (dic_permitidas,list_prohibidas) =funciones.procesarPalabras(palabras)
-
-listaLP = funciones.listaLetras(list_prohibidas) # Lista de letras pertenecientes a las palabras prohibidas
-
-# dfa = new Graph()
-# dfa = funciones.dfaPalabras(listaLP) # DFA formado con las letras de las palabras prohibidas
-
-# palabras_LP=funciones.convertirA_ER(dfa) # Expresion regular de palabras formadas con las letras de palabras prohibidas.
-
-# ER para representar cualquier caracter menos las que aparecen
-# en las palabras prohibidas
-#strLNP="[^"
-#for l in listaLP:
-#    strLP += l
-#str += "]"
-
-# ER para representar cualquier palabra menos las prohibidas     
-#NP= "("+strLNP+"*("+palabras_LP+strLNP+"+|"+strLNP+")*"+palabras_LP+"?)" 
 
 # Con todas sus permutaciones de palabras en diccionario 
 #ER=..NP palabra NP palabra.. 
@@ -49,7 +29,7 @@ listaLP = funciones.listaLetras(list_prohibidas) # Lista de letras perteneciente
 
 #for file in files:
 #    for line in file:
-#        if(re.match("^"+ER+"$",line):
+#        if(re.search("^"+ER+"$",line):
 #           print line
 
 # Y finnn :)
