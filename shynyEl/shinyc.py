a2ff362d7b899cde1c03bbee2ba0bf5064dd6947
unknown <Leroy@leroygordo.(none)>
2011-11-20 23:43:10 -0430

    prueba

--- shynyEl/shinyc.py
+++ shynyEl/shinyc.py
@@ -5,3 +5,11 @@
 #           Jennifer Dos Reis 08-10323
 #
 # --------------------------------------------------
+
+import lexer_parser
+
+data = "= a+a"
+
+result = parser.parse(data)
+
+print result
