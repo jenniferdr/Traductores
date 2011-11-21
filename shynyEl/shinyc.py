5ef360d8fb809154274e513e4afec239a4f60dc4
unknown <Leroy@leroygordo.(none)>
2011-11-21 10:00:25 -0430

    main listo

--- shynyEl/shinyc.py
+++ shynyEl/shinyc.py
@@ -9,18 +9,37 @@
 import lexer_parser
 import ply.lex as lex
 import ply.yacc as yacc
-
-data = "a:int := c + v"
+import sys
 
 lexer = lex.lex(module=lexer_parser)
-
-lexer.input(data)
-
-for tok in lexer:
-    print tok
-
 parser = yacc.yacc(module=lexer_parser,start='program',errorlog=yacc.NullLogger())
 
-result = parser.parse(data)
+data = sys.argv[1]
 
-print result
+file = open(data,"r")
+
+while True:
+    b = ''
+    bl = False
+    s1 = file.read(1)
+    if len(s1) == 0 :
+        break    
+    if s1 == '{':
+        s1 = file.read(1)
+        while True:
+            s1 = file.read(1)
+            if s1 == '%':
+                s1 = file.read(1)
+                if  s1 == '}':
+                    bl = True
+                    break
+                else:
+                    b = b + s1
+            else:
+                b = b + s1
+    if bl:
+        print "\n" + b
+        lexer.input(b)
+        for tok in lexer: print tok
+        result = parser.parse(b)
+        print result
